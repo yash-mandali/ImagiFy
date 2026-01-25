a9cc3f349f9a5de179b804595eb888c6a7c6926e
yash-mandali <yashmandali211@gmail.com>
2026-01-25 20:03:25 +0530

    footer links

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -8,9 +8,9 @@ const Footer = () => {
       <p className='hidden md:flex flex-1 border-l border-gray-400 pl-4 text-sm md:text-sm text-gray-600 font-medium'>Copyright @ImagiFy | All right reserved.</p>
       <p className='md:hidden text-sm text-gray-600 text-center font-medium'>Copyright @ImagiFy | All right reserved.</p>
       <div className='flex gap-3 sm:gap-4'>
-        <img src={assets.facebook_icon} className='w-8 sm:w-9 md:w-10 hover:bg-blue-600 rounded-full transition-all duration-400 hover:scale-110 cursor-pointer' alt="" />
-        <img src={assets.twitter_icon} className='w-8 sm:w-9 md:w-10 hover:bg-blue-600 rounded-full transition-all duration-400 hover:scale-110 cursor-pointer' alt="" />
-        <img src={assets.instagram_icon} className='w-8 sm:w-9 md:w-10 hover:bg-blue-600 rounded-full transition-all duration-400 hover:scale-110 cursor-pointer' alt="" />
+        <img src={assets.facebook_icon} className='w-8 sm:w-9 md:w-10 hover:bg-blue-600 rounded-full transition-all duration-400 hover:scale-110 cursor-pointer' alt="#" />
+        <img src={assets.twitter_icon} className='w-8 sm:w-9 md:w-10 hover:bg-blue-600 rounded-full transition-all duration-400 hover:scale-110 cursor-pointer' alt="#" />
+        <img src={assets.instagram_icon} className='w-8 sm:w-9 md:w-10 hover:bg-blue-600 rounded-full transition-all duration-400 hover:scale-110 cursor-pointer' alt="#" />
       </div>
     </div>
   )
