09ad0f17847baa21815ea573c853203b05a96eca
yash-mandali <yashmandali211@gmail.com>
2026-01-25 18:25:08 +0530

    btn change

--- client/src/components/GenerateBtn.jsx
+++ client/src/components/GenerateBtn.jsx
@@ -24,9 +24,21 @@ const GenerateBtn = () => {
       viewport={{ once: true }}
       className='text-center pb-16'>
       <h1 className='text-2xl md:text-3xl lg:text-4xl mt-4 font-semibold text-neutral-800 py-6 md:py-16'>See the magic. Try now</h1>
-      <button onClick={onClickHandler} className='inline-flex items-center gap-2 lg:px-12 sm:px-5 py-3 rounded-full bg-black text-white m-auto hover:scale-105 transition-all duration-500 cursor-pointer '>Generate Images
+      {/* <button onClick={onClickHandler} className='inline-flex items-center gap-2 lg:px-12 sm:px-5 py-3 rounded-full bg-black text-white m-auto hover:scale-105 transition-all duration-500 cursor-pointer '>Generate Images
         <img src={assets.star_group} alt="" className='h-6' />
-      </button>
+      </button> */}
+      <motion.button
+        onClick={onClickHandler}
+        className='sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full cursor-pointer'
+        whileHover={{ scale: 1.05 }}
+        whileTap={{ scale: 0.95 }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ default: { duration: 0.5 }, opacity: { delay: 0.8 }, duration: 1 }}
+
+      >Generate Images
+        <img src={assets.star_group} className='h-6' alt="" />
+      </motion.button>
     </motion.div>
   )
 }
